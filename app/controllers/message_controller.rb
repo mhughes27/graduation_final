e4df359db9e800c2a242ce5cf737186469877a26
Matt Hughes <mhughes27@gmail.com>
2016-09-04 18:06:12 -0500

    routes working properly

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,22 @@
 class MessageController < ApplicationController
-  def index
+  	def index
     	@messages = Message.all
   		@message = Message.new
   	end
+
+  	def show
+		@message = Message.find(params[:id])
+	end
+
+  	def create
+		@message = Message.new(message_params)
+
+		@message.save
+		redirect_to "/"
+	end
+
+	private
+		def message_params
+    		params.require(:message).permit(:visitor, :body)
+  		end
 end
